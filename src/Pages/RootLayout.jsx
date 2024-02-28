238e9ef6707ec0ec6bbfa0244dfc31931db1e897
ahmedomar600 <54773346+ahmedomar600@users.noreply.github.com>
2024-02-28 16:01:45 +0200

    Update RootLayout.jsx

--- src/Pages/RootLayout.jsx
+++ src/Pages/RootLayout.jsx
@@ -3,7 +3,9 @@ import { Outlet } from "react-router-dom";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
-
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 
 import Header from "../comp/Header"
@@ -11,6 +13,11 @@ import Footer from "../comp/Footer"
 // import Footer from "../comp/Footer"
 
 const RootLayout = () => {
+
+    useEffect(() => {
+      Aos.init();
+    }, []);
+
   return (
     <>
       <Header />
