535cf36f6aa3df337246f8fa83a974bf46617d6f
ahmedomar600 <54773346+ahmedomar600@users.noreply.github.com>
2024-02-28 15:57:29 +0200

    Update App.jsx

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -55,7 +55,7 @@ function App() {
               <img className='img-fluid' src={img4} alt="" />
             </div>
           </div>
-          <div className="Box-Txt"data-aos="fade-down-right"data-aos-duration="1000" >
+          <div className="Box-Txt"data-aos="fade-down-right" data-aos-duration="1000">
 
             <h4>ماذا عنا</h4>
             <h2>اهلا بكم في مركز <span >خيركم</span> لتحفيظ القران الكريم</h2>
@@ -89,40 +89,38 @@ function App() {
           </div>
           <div className="Content">
             <div className="Cards">
-              <div className="Box-Card-1 "data-aos="flip-left"data-aos-duration="1000"
-                
-                >
-                <div className="Card-1"                          >
+              <div className="Box-Card-1 ">
+                <div className="Card-1" data-aos="flip-left" data-aos-duration="1000"                          >
                   <img src={Ser1} alt="" />
                   <h2>تحفيظ القرآن الكريم</h2>
                   <p>نعمل علي تدريب الطلاب على تحفيظ القرآن والمراجعة المنتظمة</p>
                 </div>
-                <div className="Card-2">
+                <div className="Card-2" data-aos="flip-right" data-aos-duration="1000"> 
                   <img src={Ser2} alt="" />
                   <h2>تجويد القرآن الكريم</h2>
                   <p>نعمل ع تدريس قواعد التجويد والمخارج الصوتية لتحسين جودة التلاوة</p>
                 </div>
               </div>
               <div className="Box-Card-1">
-                <div className="Card-1" data-aos="flip-up" data-aos-duration="1000">
+                <div className="Card-1" data-aos="fade-up-left" data-aos-duration="1000">
                   <img src={Ser3} alt="" />
                   <h2 >علم النحو والصرف</h2>
                   <p>تُعنى بتعليم النحو والصرف لفهم بنية الجمل والكلمات في القرآن</p>
                 </div>
-                <div className="Card-2">
+                <div className="Card-2" data-aos="fade-up-right" data-aos-duration="1000">
                   <img src={Ser4} alt="" />
                   <h2>القواعد القرآنية</h2>
                   <p>نقوم بتعليم القواعد القرآنية لفهم النصوص بشكل أفضل</p>
                 </div>
               </div>
               <div className="Box-Card-1">
-                <div className="Card-1">
+                <div className="Card-1" data-aos="zoom-in" data-aos-duration="1000">
                   <img src={Ser5} alt="" />
                   <h2>نور البيان والقاعدة النورانية</h2>
                   <p>تم تصميم هذه الدورة كخطوة أولى في تعلم القرآن للأطفال</p>
                 </div>
-                <div className="Card-2">
-                  <img src={Ser6} alt="" />
+                <div className="Card-2" data-aos="zoom-in" data-aos-duration="1000">
+                  <img src={Ser6} alt="" / >
                   <h2>الأخلاق والتربية الإسلامية</h2>
                   <p>تسعى إلى تعزيز القيم والأخلاق الإسلامية خلال عملية التحفيظ</p>
                 </div>
